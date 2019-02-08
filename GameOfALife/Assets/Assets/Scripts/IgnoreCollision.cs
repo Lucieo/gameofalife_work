c97b794f87d221cebb29b92893a9fe91d4ae0392
aTabarly <luciecamilleory@gmail.com>
2019-02-08 18:11:40 +0100

    fadein fade out +mouvements anais

--- GameOfALife/Assets/Assets/Scripts/IgnoreCollision.cs
+++ GameOfALife/Assets/Assets/Scripts/IgnoreCollision.cs
@@ -5,13 +5,8 @@ public class IgnoreCollision : MonoBehaviour {
     [SerializeField]
     private Collider2D other;
 
-	// Use this for initialization
-	private void Awake () {
-        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), other, true);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
 }
