f2b2871c51182c14912c0de4ba6cbccbc4106442
aTabarly <luciecamilleory@gmail.com>
2019-02-13 22:54:02 +0100

    ajout distributeurs

--- GameOfALife/Assets/IgnoreAllButPlayer.cs
+++ GameOfALife/Assets/IgnoreAllButPlayer.cs
@@ -6,7 +6,7 @@ public class IgnoreAllButPlayer : MonoBehaviour {
    private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        if(collision.gameObject.tag!="Player" && collision.gameObject.tag !="Floor")
+        if(collision.gameObject.tag!="Player" && collision.gameObject.tag !="Floor" && gameObject.tag!=collision.gameObject.tag)
         {
             Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision.gameObject.GetComponent<Collider2D>(), true);
         }
